1c5716e11df9b94b8d497560e71df39adeb4bdc3
Abudara25 <gherari.abdallah@gmail.com>
2026-07-29 20:22:30 +0200

    Utilise le domaine yb-couverture.fr comme URL du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://y-bcouverture.vercel.app',
+  site: 'https://yb-couverture.fr',
   integrations: [sitemap()],
 });
